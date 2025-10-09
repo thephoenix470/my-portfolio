9a85fadbbca48ec055e09f607059769e5f730395
The Phoenix <thephoenix470@gmail.com>
2025-10-09 16:22:23 +0300

    Sixth commit

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,9 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-    output: 'export',
-    images:
-    {unoptimized: true}
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
