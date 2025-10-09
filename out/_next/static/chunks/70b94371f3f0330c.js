ff1fc6054f6d1bb2e8f363b4ade4ec8818fa098c
The Phoenix <thephoenix470@gmail.com>
2025-10-09 17:52:18 +0300

    Add static export files

---
+++ out/_next/static/chunks/70b94371f3f0330c.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/e60ef129113f6e24.js",
+  "static/chunks/7bf2b8282ac26f68.js",
+  "static/chunks/turbopack-57fa2643ac27bb4a.js"
+])
---
+++ out/_next/static/chunks/7dfa05600b556eb8.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_error", [
+  "static/chunks/17722e3ac4e00587.js",
+  "static/chunks/7bf2b8282ac26f68.js",
+  "static/chunks/turbopack-130ca68c8bb2b107.js"
+])
