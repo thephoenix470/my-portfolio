66275570674e5718fae199c902af40398406e4c5
The Phoenix <thephoenix470@gmail.com>
2025-10-16 11:13:39 +0300

    SEO added

--- src/components/SEO.tsx
+++ src/components/SEO.tsx
@@ -8,11 +8,14 @@ interface SEOProps {
 export default function SEO({ title, description }: SEOProps) {
   const siteName = "Innovative Solutions - TrevaCore";
   const defaultDescription = "TrevaCore - Innovative web solutions, low-code integration, and application care.";
+  const baseUrl = "https://www.trevacore.com"
 
   return (
     <>
       <title>{title ? `${title} | ${siteName}` : siteName}</title>
       <meta name="description" content={description || defaultDescription} />
+      <meta name="google-site-verification" content="nxNvIKSePTV_8ra7Imap4_rD_xPJA7Wl0-jbdMLWmG8" />
+      <link rel="canonical" href={baseUrl} />
     </>
   );
 }
